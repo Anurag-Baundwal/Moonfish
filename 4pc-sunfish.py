a0c944c599a965fa46b8b253354370dc4028ee6c
Nicholas <47761650+omatamix@users.noreply.github.com>
2023-03-28 15:43:53 -0500

    Redo search class

--- 4pc-sunfish.py
+++ 4pc-sunfish.py
@@ -170,23 +170,25 @@ class Position(namedtuple('Position', 'board score color')):
             print(
                 "+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n")
             r = ""
-
-
 class Search:
     def __init__(self):
         self.nodes = 0
         self.tt_score = {}
         self.tt_move = {}
-
+        self._ply = 0
+        self._sel_depth = 0
     def preform(self, pos):
         self.nodes = 0
         self.tt_score = {}
+        self._ply = 0
         best_move = ()
         for depth in range(1, 1000):
             best = alpha = -infinity
             beta = -alpha
             for move in self.sorted(pos):
-                score = -self.search(pos.move(move), -beta, -alpha, depth-1, 1)
+                self._ply += 1
+                score = -self.search(pos.move(move), -beta, -alpha, depth - 1)
+                self._ply -= 1
                 if score >= beta:
                     best_move = move
                     best = score
@@ -195,41 +197,33 @@ class Search:
                     best_move = move
                     best = score
                     alpha = max(alpha, score)
-            yield best_move, depth, best
-
-    def search(self, pos, alpha, beta, depth=3, ply=1):
+            yield best_move, depth, self._sel_depth, best
+    def search(self, pos, alpha, beta, depth=3):
         self.nodes += 1
-        if pos.dead():
-            return -mate+ply
-        mating_value = mate-ply
+        if (self._ply > self._sel_depth) self._sel_depth = self._ply;
+        if pos.dead(): return -mate + self._ply
+        mating_value = mate - self._ply
         if mating_value < beta:
             beta = mating_value
-            if alpha >= mating_value:
-                return mating_value
-        mated_value = -mate+ply
+            if alpha >= mating_value: return mating_value
+        mated_value = -mate + self._ply
         if mated_value > alpha:
             alpha = mated_value
-            if beta <= mated_value:
-                return mated_value
+            if beta <= mated_value: return mated_value
         depth = max(depth, 0)
         entry = self.tt_score.get(pos.hash())
         if entry and entry[1] >= depth:
-            if entry[0] == 0:
-                return entry[2]
-            elif entry[0] == -1:
-                alpha = max(alpha, entry[2])
-            elif entry[1] == 1:
-                beta = min(beta, entry[2])
-            if alpha >= beta:
-                return entry[2]
-        if depth <= 0:
-            return self.qsearch(pos, alpha, beta, ply+1)
+            if entry[0] == 0: return entry[2]
+            elif entry[0] == -1: alpha = max(alpha, entry[2])
+            elif entry[1] == 1: beta = min(beta, entry[2])
+            if alpha >= beta: return entry[2]
+        if depth <= 0: return self.qsearch(pos, alpha, beta, ply+1)
         o_alpha = alpha
         best = -infinity
-        if len(pos.gen_moves()) < 2:
-            depth += 1
         for move in self.sorted(pos):
-            score = -self.search(pos.move(move), -beta, -alpha, depth-1, ply+1)
+            self._ply += 1
+            score = -self.search(pos.move(move), -beta, -alpha, depth - 1)
+            self._ply -= 1
             if score >= beta:
                 self.tt_move[pos.hash()] = move
                 best = score
@@ -238,67 +232,28 @@ class Search:
                 self.tt_move[pos.hash()] = move
                 best = score
                 alpha = max(alpha, score)
-        if best <= o_alpha:
-            self.tt_score[pos.hash()] = (1, depth, best)
-        elif best >= beta:
-            self.tt_score[pos.hash()] = (-1, depth, best)
-        else:
-            self.tt_score[pos.hash()] = (0, depth, best)
+        if best <= o_alpha: self.tt_score[pos.hash()] = (1, depth, best)
+        elif best >= beta: self.tt_score[pos.hash()] = (-1, depth, best)
+        else: self.tt_score[pos.hash()] = (0, depth, best)
         return best
-
     def qsearch(self, pos, alpha, beta, ply):
         self.nodes += 1
-        if pos.dead():
-            return -mate+ply
+        if pos.dead(): return -mate + self._ply
         score = pos.score
-        if score >= beta:
-            return beta
-        if score < alpha-delta_margin:
-            return alpha
+        if score >= beta: return beta
+        if score < alpha - delta_margin: return alpha
         alpha = max(alpha, score)
         for move in self.sorted(pos):
-            if pos.board[move[1]] == 0:
-                continue
+            if pos.board[move[1]] == 0: continue
+            self._ply += 1
             score = -self.qsearch(pos.move(move), -beta, -alpha, ply+1)
-            if score >= beta:
-                return beta
-            if score > alpha:
-                alpha = score
+            self._ply -= 1
+            if score >= beta: return beta
+            if score > alpha: alpha = score
         return alpha
-
     def sorted(self, pos):
         killer = self.tt_move.get(pos.hash())
-        if killer:
-            yield killer
+        if killer: yield killer
         for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
-            if move == killer:
-                # If this move matches the killer then continue, we have already yielded the killer move.
-                continue
+            if move == killer: continue
             yield move
-
-
-def main():
-    search = Search()
-    history = [Position(initial, 0, 0)]
-    history[-1].render()
-    x = 0
-    while True:
-        # Fire the engine up and search for a move.
-        start = time.time()
-        for move, depth, score in search.preform(history[-1]):
-            if time.time()-start > 6:
-                break
-        history.append(history[-1].move(move))
-        history[-1].render()
-        print("Depth Reached:"+str(depth)+"\n")
-        print("Nodes Reached:"+str(search.nodes)+"\n")
-        if history[-1].color in (1, 3):
-            print("Eval Score: "+str(-score)+"\n")
-        else:
-            print("Eval Score: "+str(score)+"\n")
-        if history[-1].dead():
-            break
-
-
-if __name__ == '__main__':
-    main()
